1deb7dcf01ce2315462db793ada33721574a38df
vladislav <git13vl@gmail.com>
2020-09-13 23:50:12 +0200

    Initial Commit

---
+++ SimpleApp4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp4
+//
+//  Created by Vladislav on 13/09/2020.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
